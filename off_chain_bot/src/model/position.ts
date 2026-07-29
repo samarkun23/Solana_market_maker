a5495c60486f8f43585525559ff92c34e99c2695
samarkun <samarkun4@gmail.com>
2026-07-29 20:33:33 +0530

    position interface

---
+++ off_chain_bot/src/model/position.ts
@@ -0,0 +1,42 @@
+
+export interface PositionSnapshot {
+    positionId: string;
+    owner: string;      // wallet A : SOL/USDC and wallet B : ETH/USDC
+    poolId: string;
+
+    tokenAmint: string;
+    tokenBmint: string;
+    tokenADecimal: number;
+    tokenBDecimal: number;
+
+    createdAt: number;
+    entryPrice: number;
+    entryTick: number;
+
+    tokenAAmount: bigint;
+    tokenBAmount: bigint;
+
+    enteryValueUSD: number;
+
+    lowerTick: number;
+    upperTick: number;
+
+    liquidity: bigint;
+
+    feeGrowthInsideA: bigint;
+    feeGrowthInsideB: bigint;
+
+    txSignature: string;
+    creationGasCost: number;
+
+    strategyId: string;
+    strategyName: string;
+    status: "ACTIVE" | "CLOSED" | "OUT_OF_RANGE" | "REBALANCED";
+
+    rebalanceGasCost?: number;
+    rebalanceAt?: number;
+    rebalanceReason?: string;
+
+    nextSnapShortId?: string;
+
+}
